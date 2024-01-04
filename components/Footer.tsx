53baaef49af6160a74b45f43df7d6b630b51bec8
Abdou Mehellel <abdoumeh421@gmail.com>
2024-01-04 18:52:55 +0000

    changed footer text size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <footer className="footer fixed w-full bottom-0 font-mono italic text-sm md:text-lg text-[#2fd0a8] text-center py-1 bg-[#a82fd0]">
+    <footer className="footer fixed w-full bottom-0 font-mono italic text-sm text-[#2fd0a8] text-center py-1 bg-[#a82fd0]">
       {"const hardwork = () :Promise<results> => {...}"}
     </footer>
   );
